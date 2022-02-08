63a2fbc2a295ac9479d0922775e2f64992ba40e9
Isaac Gifford <isaac.gifford@octanner.com>
2022-02-08 13:04:04 -0700

    feat: add register page

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { Route, Routes } from 'react-router-dom';
 import Header from './components/Header';
 import Home from './components/Home';
 import Login from './components/Login';
+import Register from './components/Register';
 
 const App = () => (
   <>
@@ -12,6 +13,7 @@ const App = () => (
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
       </Routes>
     </Box>
   </>
