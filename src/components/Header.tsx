a07319a7f4b2d79c35e525a3cd1ec57ba26eb226
Isaac Gifford <isaac.gifford@octanner.com>
2022-02-08 12:28:18 -0700

    refactor: update header styles

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,16 +1,41 @@
-import { AppBar, Button, Toolbar, Typography } from '@mui/material';
+import { AppBar, Box, Button, Toolbar, Typography } from '@mui/material';
 import { Link } from 'react-router-dom';
+
+const flexContainerStyles = {
+  maxWidth: '200px',
+  flex: '1 1 auto',
+};
 
 const Header = () => {
   return (
     <AppBar position="fixed">
-      <Toolbar>
-        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-          VBB Web Portal
+      <Toolbar sx={{ justifyContent: 'space-between' }}>
+        <Typography
+          variant="h6"
+          component="div"
+          sx={(theme) => ({
+            ...flexContainerStyles,
+            [theme.breakpoints.down('sm')]: { display: 'none' },
+          })}
+        >
+          Village Book Builders
         </Typography>
-        <Button color="inherit" component={Link} to="/login">
-          Login
-        </Button>
+        <Typography
+          variant="h6"
+          component="div"
+          align="center"
+          sx={flexContainerStyles}
+        >
+          Village Portal
+        </Typography>
+        <Box display="flex" justifyContent="flex-end" sx={flexContainerStyles}>
+          <Button color="inherit" component={Link} to="/login">
+            Login
+          </Button>
+          <Button color="inherit" component={Link} to="/register">
+            Register
+          </Button>
+        </Box>
       </Toolbar>
     </AppBar>
   );
