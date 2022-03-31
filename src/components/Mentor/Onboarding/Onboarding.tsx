abb7738bfb1fd4eb258b1250f8e4a0d7a8e674df
Mengzhe Zhang <zhang.mengz@husky.neu.edu>
2022-03-31 19:40:37 -0400

    first onboarding page

--- src/components/Mentor/Onboarding/Onboarding.tsx
+++ src/components/Mentor/Onboarding/Onboarding.tsx
@@ -1,22 +1,57 @@
 import MentorHeader from '../MentorHeader'; 
-import { Button, Grid, Paper, TextField, Typography } from '@mui/material';
+import { Checkbox, FormControlLabel, FormGroup, Button, Grid, Paper, TextField, Typography, Box, Container, styled, } from '@mui/material';
 import { Link } from 'react-router-dom';
+// import{ ArrowCircleRightIcon, FiberManualRecordIcon, FiberManualRecordOutlinedIcon } from '@mui/icons-material';
 import ArrowCircleRightIcon from '@mui/icons-material/ArrowCircleRight';
+import FiberManualRecordIcon from '@mui/icons-material/FiberManualRecord';
+import FiberManualRecordOutlinedIcon from '@mui/icons-material/FiberManualRecordOutlined';
+
+const Item = styled(Paper)(({ theme }) => ({
+  padding: theme.spacing(1),
+  height: 500,
+  textAlign: 'center',
+  color: theme.palette.text.secondary,
+}));
 
 const Onboarding = () => {
     return (
         <>
       <MentorHeader/>
 
-      <Grid item xs={12}>
-          <Typography variant="h4">Training</Typography>
-      </Grid>
+  <Grid container >
+      <Grid item xs={8} >
+      <Item>
 
-      <Grid>
+      <Typography variant="h4">View mentor training resources</Typography>
+      
+      <Box sx={{
+        width: 300,
+        ml: 23,
+        mt:10,
+        mb:10,
+      }}>
+      <FormGroup>
+      <FormControlLabel control={<Checkbox />}  label="Google Doc:" />
+      <FormControlLabel control={<Checkbox  />} label="Notion:" />
+      </FormGroup>
+      </Box>
+
       <Button component={Link} to="/mentor/onboardingDona">
       NEXT<ArrowCircleRightIcon/>
     </Button>
+    </Item>
     </Grid>
+
+ 
+    <Grid item xs={4}>
+    <Item>
+      <p><FiberManualRecordIcon/><FiberManualRecordOutlinedIcon/><FiberManualRecordOutlinedIcon/><FiberManualRecordOutlinedIcon/><FiberManualRecordOutlinedIcon/><FiberManualRecordOutlinedIcon/></p>
+    <Typography variant="h3">Training</Typography>
+    </Item>
+    </Grid>
+
+  </Grid>
+    
     </>
       );
   };
