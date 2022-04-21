d46bd9d100229f531ad4edd148175f557274d9e4
Mengzhe Zhang <zhang.mengz@husky.neu.edu>
2022-04-21 14:40:40 -0400

    update mentor profile page

--- src/components/Mentor/Profile/Profile.tsx
+++ src/components/Mentor/Profile/Profile.tsx
@@ -1,89 +1,173 @@
 // import * as React from 'react';
 import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
-import { Checkbox, FormControlLabel, FormGroup, Button, Grid, Paper, TextField, Typography, Box, Container, styled } from '@mui/material';
-// import SelectTimezoneMaterialUi from 'select-timezone-material-ui';
-// import TimezoneSelect from 'react-timezone-select';
-// import TimezonePicker from 'react-bootstrap-timezone-picker';
-// import 'react-bootstrap-timezone-picker/dist/react-bootstrap-timezone-picker.min.css';
-
+import { Checkbox, FormControlLabel, FormControl, FormGroup, Button, Grid, Paper, TextField, Typography, Box, Container, styled, Radio, RadioGroup, FormLabel } from '@mui/material';
 import OpportunitySelect from './OpportunitySelect';
-import SelectTopic from '../Profile/SelectTopic';
+import LanguageDropdown from '../../LanguageDropdown';
+import CareerDropdown from '../../CareerDropdown';
+import SubjectDropdown from '../../SubjectDropdown';
+import TimezonesDropdown from '../../TimezoneSelect';
+
 import MentorHeader from '../MentorHeader'; 
 import MentorSideBar from '../MentorSideBar';
+
+const defaultForm = {
+  careers: [] as number[],
+  mentoringLanguages: [] as number[],
+  // should be a list from all subjects in the backend
+  subjects: [] as number[],
+  applicationVideoUrl: '',
+  interests: '',
+  phoneNumber: '',
+  secondaryEmail: '',
+  corporateCode: '',
+  isOfAge: false,
+  timezone: '',
+};
 
 
 
 const Profile = () => {
-  const [selectedTimezone, setSelectedTimezone] = useState({})
 
+const [formValue, setFormValue] = useState(defaultForm);
 
     return (
       <>
       <MentorHeader/>
       <MentorSideBar/>
+
       <Box sx={{
          ml: 20,
-         mt: 15,
-         mr: 15
+         mt: 10,
+         mr: 20,
+         mb: 10,
       }}>
+
+  <Grid container spacing={3}>
+    <Grid item xs={12}>
       <Typography variant='h6'>Video Upload</Typography>
-      <Typography variant='body1'>Please uploa  a 2-3 mins video:</Typography>
+      <Typography variant='body1'>Please Upload a 2-3 mins video:</Typography>
       <Grid container spacing={3} sx={{mt:2}}>
-        <Grid item xs={3}><Button variant="outlined">YouTube</Button></Grid>
-        <Grid item xs={3}><Button variant="outlined">Google Drive</Button></Grid>
-        <Grid item xs={3}><Button variant="outlined">Drop Box</Button></Grid>
+        <Grid item xs={3}><Button variant="outlined" href='https://studio.youtube.com/channel'>YouTube</Button></Grid>
+        <Grid item xs={3}><Button variant="outlined" href='https://drive.google.com/drive/my-drive'>Google Drive</Button></Grid>
+        <Grid item xs={3}><Button variant="outlined" href='https://www.dropbox.com/home'>Drop Box</Button></Grid>
+        </Grid>
       </Grid>
 
-      <p><TextField id="standard-basic" label="URL:" variant="standard" />
-      </p>
-      <p><TextField id="standard-basic" label="Year of Birth:" variant="standard" />
-      </p>
+      <Grid item xs={12}>
+      <TextField id="standard-basic" label="URL:" variant="standard" />
+      </Grid>
+      <Grid item xs={12}>
+      <TextField id="standard-basic" label="Year of Birth:" variant="standard" />
+      </Grid>
+      
+      <Grid item xs={12} sx={{mt:5}}>
+      <FormControl>
+<Typography variant='h6' >Do you have any crimes or misdemeanors?</Typography>
+  <RadioGroup 
+  row
+  aria-labelledby="demo-row-radio-buttons-group-label"
+  name="row-radio-buttons-group">
+        <FormControlLabel  value="yes" control={<Radio/>} label="YES" />
+        <FormControlLabel  value="no" control={<Radio/>} label="NO" />
+  </RadioGroup>
+  </FormControl>
+      </Grid>
 
-      <Typography variant='h6' sx={{mt:5}}>Do you have any crimes or misdemeanors?</Typography>
-      <FormGroup  row>
-        <FormControlLabel  control={<Checkbox />} label="YES" />
-        <FormControlLabel  control={<Checkbox />} label="NO" />
-      </FormGroup>
-      <Typography variant='body1' sx={{mt:2}}>If YES, please list every record:</Typography>
-      <p><TextField id="standard-basic"  variant="standard" />
-      </p>
+      <Grid item xs={12} sx={{mt:2}}>
+      <Typography variant='body1' >If YES, please list every record:</Typography>
+      <TextField id="standard-basic"  variant="standard" />
+    </Grid>
 
-      <Typography variant='h6' sx={{mt:5}}>How do you find out this opportunity?</Typography>
+    <Grid item xs={12}  sx={{mt:5}}>
+      <Typography variant='h6'>How do you find out this opportunity?</Typography>
      <OpportunitySelect/>
+</Grid>
 
-     <Typography variant='h6' sx={{mt:5}}>Time Zone</Typography>
-     {/* <TimezoneSelect
-          // value='Africa/Casablanca'
-          // value={selectedTimezone}
-          value = ''
-          onChange={setSelectedTimezone}
-        /> */}
-<Typography variant='h6'>Which languages would you be comfortable mentoring in?</Typography>
-<Typography variant='h6'>Which topics would you be comfortabele tororing a student in?</Typography>
-<SelectTopic/>
+<Grid item xs={12} sx={{mt:5}}>
+     <Typography variant='h6' >Time Zone</Typography>
+          <TimezonesDropdown
+            selectedTimezone={formValue.timezone}
+            handleSelectTimezone={(timezone) =>
+              setFormValue({ ...formValue, timezone: timezone })
+            }
+          />
+        </Grid>
 
-<Typography variant='h6'>Which careers/professional disciplines are you familiar with or have experience in?  </Typography>
-<Typography variant='h6'>What other subjects or careers do you have experience with or knowledge about? </Typography>
+<Grid item xs={12} sx={{mt:5}}>
+<Typography variant='h6' >Which languages would you be comfortable mentoring in?</Typography>
+<LanguageDropdown  
+selectedLanguages={formValue.mentoringLanguages}
+handleSelectLanguages={(languageIds) =>
+setFormValue({ ...formValue, mentoringLanguages: languageIds })
+            }/>
+  </Grid>
+
+  <Grid item xs={12} sx={{mt:5}}>
+<Typography variant='h6' >Which topics would you be comfortabele tororing a student
+ in?</Typography>
+          <SubjectDropdown
+            selectedSubjects={formValue.subjects}
+            handleSelectSubjects={(subjectIds) =>
+              setFormValue({ ...formValue, subjects: subjectIds })
+            }
+          />
+        </Grid>
+
+<Grid item xs={12} sx={{mt:5}}>
+<Typography variant='h6' >Which careers/professional disciplines are you familiar with or 
+have experience in?  </Typography>
+<CareerDropdown
+            selectedCareers={formValue.careers}
+            handleSelectCareers={(careerIds) =>
+              setFormValue({ ...formValue, careers: careerIds })
+            }
+          />
+  </Grid>
+
+  <Grid item xs={12} sx={{mt:5}}>
+<Typography variant='h6' >What other subjects or careers do you have experience with 
+or knowledge about? </Typography>
 <TextField
           id="outlined-multiline-static"
           label="Please enter here..."
           multiline
           rows={4}
         />
-<Typography variant='h6'>Please indicate whether you would like to mentor via Microsoft Teams or Google Meets. </Typography>
-<FormGroup >
-        <FormControlLabel  control={<Checkbox />} label="Microsoft Teams"/>
-        <FormControlLabel  control={<Checkbox />} label="Google Meet" />
-  </FormGroup>
+  </Grid>
 
+  <Grid item xs={12} sx={{mt:5}}>
+  <FormControl>
+ <Typography variant='h6' >Please indicate whether you would like to mentor via Microsoft Teams or Google Meets</Typography>
+<FormGroup ></FormGroup>
+  <RadioGroup
+    aria-labelledby="demo-radio-buttons-group-label"
+    defaultValue="GoogleMeet"
+    name="radio-buttons-group">
+    <FormControlLabel value="GoogleMeet" control={<Radio />} label="Google Meet" />
+    <FormControlLabel value="MicrosoftTeams" control={<Radio />} label="Microsoft Teams" />
+  </RadioGroup>
+</FormControl>
+  </Grid>
+
+  <Grid item xs={12} sx={{mt:5}}>
+  <FormControl>
 <Typography variant='h6'> Once you book a mentoring session, will you be able to meet with your student consistently every week for at least 3 months?
 </Typography>
-<FormGroup  row>
-        <FormControlLabel  control={<Checkbox />} label="YES" />
-        <FormControlLabel  control={<Checkbox />} label="NO" />
-  </FormGroup>
-
+  <RadioGroup 
+  row
+  aria-labelledby="demo-row-radio-buttons-group-label"
+  name="row-radio-buttons-group">
+        <FormControlLabel  value="yes" control={<Radio/>} label="YES" />
+        <FormControlLabel  value="no" control={<Radio/>} label="NO" />
+  </RadioGroup>
+  </FormControl>
+  </Grid>
+<Grid item xs={3} sx={{mt:5}}>
+  <Button variant="contained">Save</Button></Grid>
+  <Grid item xs={3} sx={{mt:5}}>
+  <Button variant="contained">Submit</Button></Grid>
+  </Grid>
       </Box>
       </>
       );      
