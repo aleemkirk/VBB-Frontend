6dc10caef8784fed85a36db541bff2129470296a
Will Ley <wley3337@gmail.com>
2022-05-03 18:46:00 -0400

    Update error state

--- src/redux/errors/errors.actions.ts
+++ src/redux/errors/errors.actions.ts
@@ -1,5 +1,16 @@
-import { ClearErrorsActionType, CLEAR_ERRORS } from './errors.types';
+import {
+  ClearErrorsActionType,
+  CLEAR_ERRORS,
+  Errors,
+  SetErrorsActionType,
+  SET_ERRORS,
+} from './errors.types';
 
 export const clearErrors = (): ClearErrorsActionType => ({
   type: CLEAR_ERRORS,
+});
+
+export const setErrors = (payload: Errors): SetErrorsActionType => ({
+  type: SET_ERRORS,
+  payload,
 });
--- src/redux/errors/errors.reducer.ts
+++ src/redux/errors/errors.reducer.ts
@@ -4,8 +4,8 @@ export const initErrors = {
   loginErrors: {
     message: '',
   },
-  mentorRegistrationErrors: {
-    email: '',
+  mentorSignUpErrors: {
+    message: '',
   },
 };
 
