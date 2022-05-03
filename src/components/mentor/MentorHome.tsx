c2230a0916dade5a8b5bf4fd0dbf1540c765d29e
Aleem <aleemkirk@gmail.com>
2022-05-03 12:59:15 -0400

    added headers fro mentor name and created new comp for empty message

--- src/components/mentor/MentorHome.tsx
+++ src/components/mentor/MentorHome.tsx
@@ -2,16 +2,31 @@ import { Card, Grid, Typography } from '@mui/material';
 import { Link } from 'react-router-dom';
 import { useState } from 'react';
 
+const EmptySessionMsg = () => {
+
+    return (
+        <Grid item xs={12} pb={2}>
+            <Typography variant="body1">
+                You haven't booked any sessions yet. <Link to='/student/booking'>Book</Link> your sessions now!
+            </Typography>
+        </Grid>
+    );
+};
+
 
 const MentorHome = () => {
 
     return(
-        <Grid item xs={12} pb={2}>
-            <Typography variant="body1">
-                You haven't booked any sessions yet. <Link to='/mentor/booking'>Book</Link> 
-                your sessions with available students now!
-            </Typography>
+        <Grid container padding={2} spacing={1}>
+            <Grid item xs={12} pb={2}>
+                <Typography variant="h4">Hello (Mentor name)!</Typography>
+            </Grid>
+            <Grid item xs={12}>
+                <Typography variant="h4">Upcomming Sessions</Typography>
+            </Grid>
+            <EmptySessionMsg />
         </Grid>
+        
     );
 
 }
